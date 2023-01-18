1c9c80839e68aacd63477c4b9f5ada40193119d8
Matheus Bertemes <mathbertemes@gmail.com>
2023-01-18 16:03:51 -0300

    Sidebar complete

--- aulas/01-fundamentos-reactjs/src/App.jsx
+++ aulas/01-fundamentos-reactjs/src/App.jsx
@@ -1,20 +1,31 @@
 import { useState } from 'react'
 import { Header } from './components/Header'
 import { Post } from './Post'
+
+import styles from "./App.module.css"
 import "./global.css"
+import { Sidebar } from './components/Sidebar'
 
 function App() {
 
   return (
     <div>
       <Header />
-      <Post   
-      author="Matheus Bertemes" 
-      content="Teste propriedades componentes"/>
+      <div className={styles.wrapper}>
 
-      <Post   
-      author="Gustavo" 
-      content="Novo Post"/>
+        <Sidebar />
+
+        <main>
+        <Post   
+        author="Matheus Bertemes" 
+        content="Teste propriedades componentes"/>
+
+        <Post   
+        author="Gustavo" 
+        content="Novo Post"/>
+        </main>
+        
+      </div>
     </div>
   )
 }
