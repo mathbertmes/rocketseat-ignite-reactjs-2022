761743c914d1ce88309b3565460b33f8dfca517b
Matheus Bertemes <mathbertemes@gmail.com>
2023-02-03 16:53:30 -0300

    post properties

--- aulas/01-fundamentos-reactjs/src/components/Post.jsx
+++ aulas/01-fundamentos-reactjs/src/components/Post.jsx
@@ -1,35 +1,43 @@
+import { format, formatDistanceToNow } from "date-fns"
+import ptBR from "date-fns/locale/pt-BR"
 import { Avatar } from "./Avatar"
 import { Comment } from "./Comment"
 import styles from "./Post.module.css"
 
-export function Post(props){
+export function Post({ author, publishedAt, content }){
+
+  const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'ás' HH:mm'h'",{
+    locale: ptBR
+  })
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, { 
+    locale : ptBR,
+    addSuffix : true
+  })
+
   return(
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar src="https://avatars.githubusercontent.com/u/81433910?s=400&u=5ef6efc69c304a6367a5e55efe7e82b465281bd5&v=4"/>
+          <Avatar src={author.avatarUrl}/>
           <div className={styles.authorInfo}>
-            <strong>Matheus Bertemes</strong>
-            <span>Web developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
 
-        <time title="18 de agosto às 17:12h" datetime="2023-01-10 17:12:23">Publicado há 1h</time>
+        <time title={publishedDateFormatted} datetime={publishedAt.toISOString()}>
+          {publishedDateRelativeToNow}
+        </time>
       </header>
 
       <div className={styles.content}>
         
-        <p>Fala galeraa 👋</p>
-
-        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-
-        <p><a href="#">jane.design/doctorcare</a></p>
-
-        <p>
-          <a href="">#novoprojeto</a>{' '}
-          <a href="">#nlw </a>{' '}
-          <a href="">#rocketseat</a>
-        </p>
+        {content.map(content => {
+          return(
+            content.type === "paragraph" ? <p>{content.content}</p> : <p><a href="#">{content.content}</a></p>
+          )
+        })}
         
       </div>
 
